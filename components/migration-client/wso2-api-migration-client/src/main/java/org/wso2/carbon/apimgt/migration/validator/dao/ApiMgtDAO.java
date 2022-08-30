defc51ae0b9c63a82b19b7c1abcd833caea804ff
Lakith <lakith@wso2.com>
2022-08-30 20:39:13 +0530

    add api resource level auth scheme prevalidation step

--- components/migration-client/wso2-api-migration-client/src/main/java/org/wso2/carbon/apimgt/migration/validator/dao/ApiMgtDAO.java
+++ components/migration-client/wso2-api-migration-client/src/main/java/org/wso2/carbon/apimgt/migration/validator/dao/ApiMgtDAO.java
@@ -1,15 +1,23 @@
 package org.wso2.carbon.apimgt.migration.validator.dao;
 
-import org.wso2.carbon.apimgt.impl.dao.constants.SQLConstants;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.apimgt.api.model.URITemplate;
 import org.wso2.carbon.apimgt.impl.utils.APIMgtDBUtil;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
+import org.wso2.carbon.apimgt.migration.validator.dao.constants.SQLConstants;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ApiMgtDAO {
+    private static final Log log = LogFactory.getLog(ApiMgtDAO.class);
+
     private static ApiMgtDAO INSTANCE = null;
 
     public static ApiMgtDAO getInstance() {
@@ -37,5 +45,38 @@ public class ApiMgtDAO {
             }
         }
         return id;
+    }
+
+    public Set<URITemplate> getURITemplatesByAPIID(int apiId) {
+
+        Set<URITemplate> urlTemplates = new HashSet<>();
+
+        try (Connection connection = APIMgtDBUtil.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(SQLConstants.GET_URL_TEMPLATES_BY_API_ID_SQL_260)) {
+                ps.setInt(1, apiId);
+                try (ResultSet resultSet = ps.executeQuery();) {
+                    while (resultSet.next()) {
+                        String script = null;
+                        URITemplate uriTemplate = new URITemplate();
+                        uriTemplate.setUriTemplate(resultSet.getString("URL_PATTERN"));
+                        uriTemplate.setHTTPVerb(resultSet.getString("HTTP_METHOD"));
+                        uriTemplate.setAuthType(resultSet.getString("AUTH_SCHEME"));
+                        uriTemplate.setThrottlingTier(resultSet.getString("THROTTLING_TIER"));
+                        InputStream mediationScriptBlob = resultSet.getBinaryStream("MEDIATION_SCRIPT");
+                        if (mediationScriptBlob != null) {
+                            script = APIMgtDBUtil.getStringFromInputStream(mediationScriptBlob);
+                            if (script.isEmpty()) {
+                                script = null;
+                            }
+                        }
+                        uriTemplate.setMediationScript(script);
+                        urlTemplates.add(uriTemplate);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Error on retrieving URLTemplates for apiResourceLevelAuthSchemeValidation validation", e);
+        }
+        return urlTemplates;
     }
 }
