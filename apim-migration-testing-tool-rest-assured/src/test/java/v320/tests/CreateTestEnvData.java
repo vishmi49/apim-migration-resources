78c8d80160741b954f689410272ab45facce2314
Bathiya Ladduwahetty <bathiya@wso2.com>
2023-01-25 15:12:40 +0530

    add create env data

--- apim-migration-testing-tool-rest-assured/src/test/java/v320/tests/CreateTestEnvData.java
+++ apim-migration-testing-tool-rest-assured/src/test/java/v320/tests/CreateTestEnvData.java
@@ -11,15 +11,8 @@ import org.testng.annotations.Test;
 
 import ataf.actions.BaseTest;
 import ataf.utils.ExcelReader;
-<<<<<<< HEAD
 import commons.testdata.AuthenticationData;
 import commons.testdata.PublisherData;
-=======
-import commons.testdata.AdminData;
-import commons.testdata.AuthenticationData;
-import commons.testdata.PublisherData;
-import commons.testdata.SOAPData;
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 import io.restassured.response.Response;
 import restapi.ApimVersions;
 import restapi.Authentication;
@@ -47,18 +40,12 @@ public class CreateTestEnvData extends BaseTest{
 	    
 	    
 	    // response Data
-<<<<<<< HEAD
 	    JSONObject testOutputData = new JSONObject();
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    String restAPI_ADPRestAPIID;
 	    String restAPI_inlineDoc_documentId;
 	    String[] mediationsPoliciesToUpdate = new String[3];
 	    
-<<<<<<< HEAD
 	    
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    @BeforeClass
 	    @Parameters({"testTenant"})
 	    public void getTenantInfo(String testTenant) throws Exception {
@@ -99,21 +86,13 @@ public class CreateTestEnvData extends BaseTest{
 	        String publisherUsername = publisherUserData.get("username")+ "@" + testTenant;
 	        String publisherUserassword = publisherUserData.get("password");
 	        authenticationObject.setPayload(AuthenticationData.getPayloadOfOwner(publisherUsername));
-<<<<<<< HEAD
 	        authenticationObject.setPayload(AuthenticationData.getPayloadOfOwner(publisherUserassword));
-=======
-	        authenticationObject.setPayload(AuthenticationData.getPayloadOfOwner(publisherUserassword)); // get admin user from excel
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	        authenticationObject.setUsername(publisherUsername);
 	        authenticationObject.setUserpassword(publisherUserassword);
 	        authenticationObject.setScopes(publisherAuthScope);
 	        publisherAccesstoken = new Authentication(authenticationObject).getAccessToken();
-<<<<<<< HEAD
 	        log("======================================================= ::: " + createUsername);
 	        log("======================================================= ::: " + publisherUsername);
-=======
-	        
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	        
 	        adminSystemScopesRestAPI = new Admin.SystemScopes(baseURL.toString(),adminAccesstoken,ApimVersions.APIM_3_2);
 	        apiCategoryIndividual = new Admin.ApiCategoryIndividual(baseURL.toString(), adminAccesstoken, ApimVersions.APIM_3_2);
@@ -128,7 +107,6 @@ public class CreateTestEnvData extends BaseTest{
 	    	log("[CreateTestEnvData] Creating data for tenant " + testTenant);
 	    }
 
-<<<<<<< HEAD
 //		@Test(dataProvider="getRolesData", priority=1)
 //		public void addRole(String roleName, String permissions) throws Exception {
 //			
@@ -247,107 +225,6 @@ public class CreateTestEnvData extends BaseTest{
 //	    	}
 //		}
 //	    
-=======
-		@Test(dataProvider="getRolesData", priority=1)
-		public void addRole(String roleName, String permissions) throws Exception {
-			
-			Response response = rUserStore.addRole(SOAPData.getAddRoleXML(roleName, permissions), tenantAdmin,false);
-			
-			String assertMessage = "Invalid status code";
-			if(response.statusCode() != 202) {
-				log(response.body().asPrettyString());
-				assertMessage = response.body().xmlPath().getString("Envelope.Body.Fault.faultstring");
-			}
-			Assert.assertEquals(assertMessage,202, response.statusCode());
-			log("Added Role : " + roleName);
-		}
-		
-	    @Test(dataProvider="getUsersData", priority=2)
-		public void addUser(String userName, String password, String roles) throws Exception {
-
-			Response response = rUserStore.addUser(SOAPData.getAaddUserSOAPXML(userName,password,roles), tenantAdmin,false);
-			String assertMessage = "Invalid status code";
-			if(response.statusCode() != 202) {
-				log(response.body().asPrettyString());
-				assertMessage = response.body().xmlPath().getString("Envelope.Body.Fault.faultstring");
-			}
-			Assert.assertEquals(assertMessage,202, response.statusCode());
-			log("Added user : " + userName);
-		}
-	    
-	    @Test(priority=3)
-		public void addRoleAliasMapping() throws Exception {    	
-	    	Response response = adminSystemScopesRestAPI.addNewRoleAlias(AdminData.getADPRoleAliesesPayload(), false);
-
-			if(response.statusCode() != 200) {
-				log(response.body().asPrettyString());
-			}
-			Assert.assertEquals("Invalid status code",200, response.statusCode());
-			log("Added scope mappings");
-		}
-	    
-	    @Test(dataProvider="getApiCategories", priority=4)
-		public void addApiCategory(String name, String description) throws Exception {  
-	    	Response response = apiCategoryIndividual.addApiCategories(AdminData.getAPICategoriesJsonPayload(name, description), false);
-
-			if(response.statusCode() != 201) {
-				log(response.body().asPrettyString());
-			}
-			Assert.assertEquals("Invalid status code",201, response.statusCode());
-			log("Added scope mappings");
-	    	//log(response.body().asPrettyString()); // get responseID from response and append to log file
-		}
-	    
-	    @Test(priority=5)
-		public void addAdvancedThrottlingPolicy() throws Exception {  
-	    	Response response = advancedPolicyCollection.addAdvancedThrottlingPolicy(AdminData.getAdvancedThrotellingPolicyADP10PerMin(), false);
-
-			if(response.statusCode() != 201) {
-				log(response.body().asPrettyString());
-			}
-			Assert.assertEquals("Invalid status code",201, response.statusCode());
-			//log(response.body().asPrettyString()); // get policyId from response and append to log file
-			log("Added Advanced Throttling Policy");
-		}
-	    
-	    @Test(priority=6)
-		public void addApplicationThrottlingPolicy() throws Exception {  
-	    	Response response = applicationPolicyCollection.addApplicationThrottlingPolicies(AdminData.getApplicationThrotellingPolicyADP5PerMin(), false);
-
-			if(response.statusCode() != 201) {
-				log(response.body().asPrettyString());
-			}
-			Assert.assertEquals("Invalid status code",201, response.statusCode());
-			//log(response.body().asPrettyString()); // get policyId from response and append to log file
-			log("Added Application Throttling Policy");
-		}
-	    
-	    @Test(priority=7)
-		public void addSubscriptionThrottlingPolicy() throws Exception {  
-	    	Response response = subscriptionPolicyCollectionAPI.addSubscriptionThrottlingPolicy(AdminData.getSubscriptionThrottlingPolicyADPBrass(), false);
-
-			if(response.statusCode() != 201) {
-				log(response.body().asPrettyString());
-			}
-			Assert.assertEquals("Invalid status code",201, response.statusCode());
-			log(response.body().asPrettyString()); // get policyId from response and append to log file
-			log("Added Application Throttling Policy");
-		}
-	    
-	    @Test(dataProvider="getScopesData", priority=8)
-		public void addSharedScopes(String name, String displayName, String description, String bindings) throws Exception {  
-	    	Response response = publisherScopesAPI.addNewSharedScopes(AdminData.getScopeJsonPayload(name, displayName, description, bindings), false);
-
-			if(response.statusCode() != 201) {
-				log(response.body().asPrettyString());
-			}
-			//log(response.body().asPrettyString()); // get policyId from response and append to log file
-			Assert.assertEquals("Invalid status code",201, response.statusCode());
-			
-			log("Added Application Throttling Policy");
-		}
-	    
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    @Test(priority=9)
 		public void createRestAPI() throws Exception {  
 	    	Response response = publisherApis.createApi(PublisherData.getAPIADPRestAPIJsonPayload(), false);
@@ -356,7 +233,6 @@ public class CreateTestEnvData extends BaseTest{
 				log(response.body().asPrettyString());
 			}
 			Assert.assertEquals("Invalid status code",201, response.statusCode());
-<<<<<<< HEAD
 			//log(response.body().asPrettyString());
 			restAPI_ADPRestAPIID = response.jsonPath().get("id");
 			log("Created Rest API " + restAPI_ADPRestAPIID);
@@ -368,24 +244,12 @@ public class CreateTestEnvData extends BaseTest{
 	    
 	    @Test(priority=10)
 		public void addNewInlineDocument() throws Exception { 
-=======
-			restAPI_ADPRestAPIID = response.jsonPath().get("id");
-			log("Created Rest API");
-		}
-	    
-	    @Test(priority=10)
-		public void addNewInlineDocument() throws Exception {  
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    	Response response = publisherApis.addNewDocToApi(restAPI_ADPRestAPIID,PublisherData.getADPInlineDocPayload(), false);
 
 			if(response.statusCode() != 201) {
 				log(response.body().asPrettyString());
 			}
 			Assert.assertEquals("Invalid status code",201, response.statusCode());
-<<<<<<< HEAD
-=======
-			//log(response.body().asPrettyString());
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 			restAPI_inlineDoc_documentId = response.jsonPath().get("documentId");
 			log("Added new inline doc type");
 			
@@ -395,7 +259,6 @@ public class CreateTestEnvData extends BaseTest{
 				log(response2.body().asPrettyString());
 			}
 			log("Added inline content");
-<<<<<<< HEAD
 			
 			// add output data
 			testOutputData.put("addNewInlineDocument", response2.body().jsonPath().get("$"));
@@ -403,11 +266,6 @@ public class CreateTestEnvData extends BaseTest{
 	    @Test(priority=11)
 		public void addMarkdownDocument() throws Exception {  
 	    	
-=======
-		}
-	    @Test(priority=11)
-		public void addMarkdownDocument() throws Exception {  
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    	Response response = publisherApis.addNewDocToApi(restAPI_ADPRestAPIID,PublisherData.getADPMarkdownDocPayload(), false);
 
 			if(response.statusCode() != 201) {
@@ -424,12 +282,9 @@ public class CreateTestEnvData extends BaseTest{
 				log(response2.body().asPrettyString());
 			}
 			log("Added markdown content");
-<<<<<<< HEAD
 			
 			// add output data
 			testOutputData.put("addMarkdownDocument", response2.body().jsonPath().get("$"));
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 		}
 	    @Test(priority=12)
 		public void addPublicForumURLDocument() throws Exception {  
@@ -440,12 +295,9 @@ public class CreateTestEnvData extends BaseTest{
 			}
 			Assert.assertEquals("Invalid status code",201, response.statusCode());
 			log("Added public forum URL Document");
-<<<<<<< HEAD
 			
 			// add output data
 			testOutputData.put("addPublicForumURLDocument", response.body().jsonPath().get("$"));
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    }
 	    @Test(priority=13)
 		public void addOtherFileDocument() throws Exception {  
@@ -458,22 +310,14 @@ public class CreateTestEnvData extends BaseTest{
 			String restAPI_fileDoc_documentId = response.jsonPath().get("documentId");
 			log("Added File Document");
 			
-<<<<<<< HEAD
 			Response response2 = publisherApis.uploadContentOfDocOfApi(restAPI_ADPRestAPIID,restAPI_fileDoc_documentId,"./src/test/resources/testdata/adp-file-doc-content.txt");
 			// add output data
 			testOutputData.put("addOtherFileDocument", response2.body().jsonPath().get("$"));
-=======
-			publisherApis.uploadContentOfDocOfApi(restAPI_ADPRestAPIID,restAPI_fileDoc_documentId,"./src/test/resources/testdata/adp-file-doc-content.txt");
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    }
 		
 	    @Test(priority=14)
 		public void getMediationPolicies() throws Exception {  
-<<<<<<< HEAD
 
-=======
-	    	
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    	Response response = publisherApis.addApiSpecificMediationPolicy(restAPI_ADPRestAPIID,"./src/test/resources/testdata/log_in_message.xml");
 
 			if(response.statusCode() != 201) {
@@ -487,14 +331,10 @@ public class CreateTestEnvData extends BaseTest{
 			mediationsPoliciesToUpdate[1] = new JSONObject(response2.jsonPath().getJsonObject("list[11]")).toString();
 			mediationsPoliciesToUpdate[2] = new JSONObject(response2.jsonPath().getJsonObject("list[15]")).toString();
 
-<<<<<<< HEAD
 			log("Get Mediation Policies");
 			
 			// add output data
 			testOutputData.put("getMediationPolicies", response2.body().jsonPath().get("$"));
-=======
-			log("Get Mediation Policies");	
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    }
 	    
 	    @Test(priority=15)
@@ -507,13 +347,10 @@ public class CreateTestEnvData extends BaseTest{
 			}
 			Assert.assertEquals("Invalid status code",200, response.statusCode());
 			log("Updated API with mediations policies");
-<<<<<<< HEAD
 			
 			// add output data
 			testOutputData.put("updateRestAPI", response.body().jsonPath().get("$"));
 			//log(testOutputData.toString());
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    }
 	    
 	    @Test(priority=16)
@@ -525,13 +362,10 @@ public class CreateTestEnvData extends BaseTest{
 			}
 			Assert.assertEquals("Invalid status code",201, response.statusCode());
 			log("Created new version");
-<<<<<<< HEAD
 			
 			// add output data
 			testOutputData.put("createNewRestAPIVersion", response.body().jsonPath().get("$"));
 			log(testOutputData.toString());
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 	    }
 	    
 	    @Test(priority=17)
@@ -543,10 +377,7 @@ public class CreateTestEnvData extends BaseTest{
 				log(response.body().asPrettyString());
 			}
 			Assert.assertEquals("Invalid status code",201, response.statusCode());
-<<<<<<< HEAD
 			log(response.body().toString());
-=======
->>>>>>> 1ab02b9d458a3b8421f5e4f4a2b2c35ca3568f88
 			log("Published API");
 	    }
 	    
